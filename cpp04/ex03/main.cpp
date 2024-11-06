34297cfa187500a57156a2c2fe0c2d4641788603
bablilayoub <ayoubbablil@gmail.com>
2024-11-06 17:32:58 +0100

    new update

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:27:53 by abablil           #+#    #+#             */
-/*   Updated: 2024/11/05 15:13:34 by abablil          ###   ########.fr       */
+/*   Updated: 2024/11/06 16:57:17 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,23 @@ int main()
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+	
 	ICharacter* me = new Character("me");
+	
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	
 	ICharacter* bob = new Character("bob");
+	
 	me->use(0, *bob);
 	me->use(1, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
+	
 	return 0;
 }
