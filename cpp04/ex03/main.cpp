059b0018fe77be1285e9a4be940d27b502ce5606
bablilayoub <ayoubbablil@gmail.com>
2024-11-08 19:37:33 +0100

    new update

--- cpp04/ex03/main.cpp
+++ cpp04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/04 15:27:53 by abablil           #+#    #+#             */
-/*   Updated: 2024/11/07 22:42:36 by abablil          ###   ########.fr       */
+/*   Updated: 2024/11/08 15:57:22 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,18 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 
+void f()
+{
+	system("leaks program");
+}
+
 int main()
 {
+	atexit(f);
+	AMateria* tmp_inventory[4];
+	for (int i = 0; i < 4; i++)
+		tmp_inventory[i] = NULL;
+	
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -26,17 +36,28 @@ int main()
 	AMateria* tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
+	tmp_inventory[0] = tmp;
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	tmp_inventory[1] = tmp;
 	
 	ICharacter* bob = new Character("bob");
 	
 	me->use(0, *bob);
 	me->use(1, *bob);
 
+	me->unequip(0);
+	me->unequip(1);
+	me->use(0, *bob);
+	me->use(1, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
-	
+	for (int i = 0; i < 4; i++)
+	{
+		if (tmp_inventory[i])
+			delete tmp_inventory[i];
+	}
 	return 0;
 }
