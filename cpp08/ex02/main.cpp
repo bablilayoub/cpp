69e61f8a950e2fe2fbd717b0ba7d5a09d160cd9a
bablilayoub <ayoubbablil@gmail.com>
2024-12-10 19:32:29 +0100

    New update

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: abablil <abablil@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 20:30:32 by abablil           #+#    #+#             */
-/*   Updated: 2024/12/10 18:34:33 by abablil          ###   ########.fr       */
+/*   Updated: 2024/12/10 19:32:18 by abablil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,21 @@ int main()
 	stack.push("you");
 	stack.push("!!!!");
 
-	std::cout << "Size : " << stack.size() << std::endl;	
+	if (stack.empty())
+	{
+		std::cout << "stack is empty" << std::endl;
+		return 0;
+	}
+
+	std::cout << "Size : " << stack.size() << std::endl;
 	stack.pop();
-	std::cout << "Size : " << stack.size() << std::endl;	
+	std::cout << "Size : " << stack.size() << std::endl;
 
 	MutantStack<std::string>::iterator begin = stack.begin();
 	MutantStack<std::string>::iterator end = stack.end();
 
 	std::cout << "Top element : " << stack.top() << std::endl;
-	
+
 	while (begin != end)
 	{
 		std::cout << *begin << std::endl;
@@ -37,27 +43,33 @@ int main()
 	return 0;
 }
 
-// int main()
-// {
-// 	MutantStack<int> stack;
-// 	stack.push(1337);
-// 	stack.push(42);
-// 	stack.push(2024);
-// 	stack.push(0);
+int main()
+{
+	MutantStack<int> stack;
+	stack.push(1337);
+	stack.push(42);
+	stack.push(2024);
+	stack.push(0);
 
-// 	std::cout << "Size : " << stack.size() << std::endl;	
-// 	stack.pop();
-// 	std::cout << "Size : " << stack.size() << std::endl;	
-
-// 	MutantStack<int>::iterator begin = stack.begin();
-// 	MutantStack<int>::iterator end = stack.end();
+	if (stack.empty())
+	{
+		std::cout << "stack is empty" << std::endl;
+		return 0;
+	}
 	
-//	std::cout << "Top element : " << stack.top() << std::endl;
+	std::cout << "Size : " << stack.size() << std::endl;
+	stack.pop();
+	std::cout << "Size : " << stack.size() << std::endl;
 
-// 	while (begin != end)
-// 	{
-// 		std::cout << *begin << std::endl;
-// 		++begin;
-// 	}
-// 	return 0;
-// }
+	MutantStack<int>::iterator begin = stack.begin();
+	MutantStack<int>::iterator end = stack.end();
+
+	std::cout << "Top element : " << stack.top() << std::endl;
+
+	while (begin != end)
+	{
+		std::cout << *begin << std::endl;
+		++begin;
+	}
+	return 0;
+}
